72a7ebbe4dcb9e585c6ae7d017954efb41a12cef
tong0820 <739068681@qq.com>
2022-05-23 11:29:00 +0800

    first commit

---
+++ 02_module/src/main.js
@@ -0,0 +1,25 @@
+import { hello } from "./util";
+import weChat from "../assets/images/weChat.png";
+import svgUrl from "../assets/images/404.svg";
+import text from "../assets/text/01.text";
+import jpgUrl from "../assets/images/bg.jpg";
+hello();
+
+const img = document.createElement("img");
+img.src = weChat;
+document.body.appendChild(img);
+
+const svg = document.createElement("img");
+svg.style.cssText = "width:400px;height:200px";
+svg.src = svgUrl;
+document.body.appendChild(svg);
+
+const block = document.createElement("div");
+block.style.cssText = "width:400px;height:200px;background:#eee";
+block.textContent = text;
+document.body.appendChild(block);
+
+const jpg = document.createElement("img");
+jpg.style.cssText = "width:400px;height:200px";
+jpg.src = jpgUrl;
+document.body.appendChild(jpg);
---
+++ 02_module/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "images/[contenthash][ext]", //统一打包生成的资源名字
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      // filename:'',
+      inject: "body",
+    }),
+  ],
+  // 资源模块
+  // asset/resource导出url   asset/inline导出资源的DataUri   asset/source 导出资源的源代码
+  // asset 通用自动选择  在resource和inline之间
+  module: {
+    rules: [
+      {
+        test: /\.png$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[contenthash][ext]", //单独打包生成的资源名字
+        },
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/inline", //data-url-base64格式
+      },
+      {
+        test: /\.text$/,
+        type: "asset/source",
+      },
+      {
+        test: /\.jpg$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+        // 资源 >8kb 的时候生成资源   资源  <8kb 的时候生成base链接
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+  },
+};
